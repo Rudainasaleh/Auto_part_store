36b85828550cb97177feef48d6d57d9458aba48c
Radina Al Rahbi <rudainasaleh14@gmail.com>
2022-07-16 13:58:50 -0500

    Update app.py

--- autostore/app.py
+++ autostore/app.py
@@ -1,4 +1,5 @@
 
+
 import curses
 from distutils.log import error
 from tkinter.tix import Form
@@ -10,18 +11,18 @@ import psycopg2.extras
 
 
 
+
+
 app = Flask(__name__)
 
 conn = psycopg2.connect(
     host = 'localhost',
-    database = '',
-    user = '',
-    password = '',
+    database = 'Autostore',
+    user = 'postgres',
+    password = 'Rudaina998642642',
     )
 
 @app.route('/')
-
-
 def index():
     return render_template('index.html')
 
@@ -38,29 +39,63 @@ def part():
 def login():
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
 
-    if (request.method == 'POST' and 'username' in request.form and 'password' in request.form) :
-        username = request.form['usernam']
+   
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
         password = request.form['password']
         print(username)
         print(password)
-
-        cursor.execute('SELECT * FROM userInfo WHERE username = %s', (username,))
+ 
+        cursor.execute('SELECT * FROM login WHERE username = %s', (username,))
         user_account = cursor.fetchone()
 
-        if user_account:
-            password_rs = user_account['password']
-            print(password_rs)
-            return redirect(url_for('/index.html'))
         
-        else:
-            flash('Incorrect username/password')
+        if user_account:
+            user_password = user_account['password']
+            print(user_password)
+            # If account exists in users table in out database
+            if ( user_password == password) :
+                # Create session data, we can access this data in other routes
 
+                return redirect(url_for('index'))
+            else:
+                # Account doesnt exist or username/password incorrect
+                flash('Incorrect username/password')
+        else:
+            # Account doesnt exist or username/password incorrect
+            flash('Incorrect username/password')
+ 
+    return render_template('login.html')    
 
 
 
 
 
     return render_template('login.html')
+
+
+@app.route('/cart.html', methods=['POST'])
+def cart():
+
+    
+    quantity = int(request.form['POST'])
+    part_quantity = int(request.form['quantity'])
+    _code = request.form['code']
+    # validate the received values
+
+
+
+    return render_template('cart.html')
+
+@app.route('/register.html')
+def register():
+
+    
+   
+
+
+
+    return render_template('register.html')
 
 
 
