35bff88f17d9a22af5bfc9605ff77f5f0600a012
Radina Al Rahbi <rudainasaleh14@gmail.com>
2022-07-20 12:52:55 -0500

    Update app.py

--- autostore/app.py
+++ autostore/app.py
@@ -1,20 +1,30 @@
 
 
+
+
 import curses
-from distutils.log import error
+
 from tkinter.tix import Form
-from flask import Flask, flash, redirect, request, url_for
+from traceback import print_exc
+
+from flask import Flask, flash, redirect, request, url_for, session
 from flask import render_template
+
+from flask import session
 import os
 import psycopg2
 import psycopg2.extras
-
+from flask_session import Session
 
 
 
 
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 conn = psycopg2.connect(
     host = 'localhost',
@@ -53,8 +63,13 @@ def register():
         if account_username:
             flash('Account already exists!')
         else:
+            
+            session['email'] = email
+            session['userid'] = account_username['userid']
+
             cursor.execute("INSERT INTO userinfo (email,username, password, first_name, last_name ) VALUES (%s,%s,%s,%s, %s)", (email, username, password, first_name, last_name))
             conn.commit()
+
             return redirect(url_for('index'))
     
     
@@ -76,7 +91,7 @@ def login():
         
         
  
-        cursor.execute('SELECT * FROM login WHERE username = %s', (username,))
+        cursor.execute('SELECT * FROM userinfo WHERE username = %s', (username,))
         user_account = cursor.fetchone()
 
         
@@ -85,9 +100,20 @@ def login():
             #print(user_password)
             # If account exists in users table in out database
             if ( user_password == password) :
+
+                cursor.execute('SELECT * FROM userinfo WHERE username = %s', (username,))
+                id = cursor.fetchone()
+                session['userid'] = id['userid']
                 
                 
 
+                
+
+
+                #cursor.execute('INSERT INTO purchase (userid) VALUES (%s)', (userid,))
+                #conn.commit()
+                #session['userid'] = userid
+           
                 return redirect(url_for('index'))
             else:
                 # Account doesnt exist or username/password incorrect
@@ -109,34 +135,59 @@ def login():
 @app.route("/parts.html", methods=['GET', 'POST'])
 def part():
 
-    
-
-
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-    cursor.execute("SELECT * from parts")
-    rows = cursor.fetchall()
+    if 'userid' in session:
 
     
-    if request.method == 'POST':
-        if request.form['add_to_cart'] == 'Add to cart':
-            partid = request.form['partid']
-            cursor.execute("SELECT * from parts WHERE partid = %s", (partid))
-            
-            
-            prudoct = cursor.fetchone()
-            name = prudoct['name']
-            image = prudoct['image']
-            price = prudoct['price']
-            quantity = request.form['quantity']
-            total_price = price 
-            
-           
+
+
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        cursor.execute("SELECT * from parts")
+        rows = cursor.fetchall()
+    
+        if request.method == 'POST':
+
+        #cursor.execute("SELECT * from parts")
+        #rows = cursor.fetchall()
+
+
         
+            if request.form['add_to_cart'] == 'Add to cart':
+
             
-            cursor.execute("INSERT INTO purches (partid, name, image, price, quantity, total_price) VALUES (%s, %s, %s, %s, %s, %s)", (partid, name, image, price, quantity, total_price))
-            conn.commit()
-            return redirect(url_for('part'))
+
+
+                    partid = request.form['partid']
+                    print(partid)
+                    cursor.execute("SELECT * from parts WHERE partid = %s", (partid,))
+            
+            
+            
+                    prudoct = cursor.fetchone()
+                #print(product)
+                    name = prudoct['name']
+                    image = prudoct['image']
+                    price = prudoct['price']
+                    quantity = request.form['quantity']
+                    total_price = price 
+
+                    userid = session['userid']
+                #id = session['userid']
+
+            
+
+                
+            
+               
+                #cursor.execute("UPDATE purchase SET partid = %s, name=%s, image=%s, price=%s, quantity=%s, total_price=%s WHERE userid =%s", (partid, name, image, price, quantity, total_price, id))
+                    cursor.execute("INSERT INTO purchase (partid, name, image, price, quantity, total_price, userid) VALUES (%s, %s, %s, %s, %s, %s, %s)", (partid, name, image, price, quantity, total_price, userid))
+                    conn.commit()
+                    return redirect(url_for('part'))
+    else:
+        return redirect(url_for('login'))
+            
+            
+                
     
     return render_template('parts.html', part = rows)
 
@@ -146,61 +197,125 @@ def part():
 def cart():
 
     
+    
 
     
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    cursor.execute("SELECT * from purches")
+
+    userid = session['userid']
+    cursor.execute("SELECT * from purchase WHERE userid = %s ", (userid,))
     rows = cursor.fetchall()
 
 
 
     if request.method == 'POST':
-        if request.form['remove'] == 'Remove':
+            
+        if request.form['remove_item'] == 'Remove':
+
+
+
             orderid = request.form['orderid']
+            
+            cursor.execute("SELECT * from parts WHERE orderid = %s", (orderid,))
+            order = cursor.fetchone()
+            partid = order['partid']
+            id = session['userid']
+            print(orderid)
+            print(id)
+            print(partid)
 
-            cursor.execute("SELECT * from purches WHERE partid = %s", (orderid))
-        
-
-            #delete_from_cart(orderid)
-            cursor.execute("DELETE FROM purches WHERE orderid = (%s) ", (orderid))
+            cursor.execute("DELETE FROM purchase WHERE partid = (%s) AND userid = (%s) AND orderid = (%s)", (partid, id, orderid,))
+            rows_deleted = cursor.rowcount
             conn.commit()
             return redirect(url_for('cart'))
 
-    cursor.execute("SELECT * from purches")
-    purches = cursor.fetchall()
+    #cursor.execute("SELECT * from purchase")
+    #purchase = cursor.fetchall()
 
-    cursor.execute("INSERT INTO CART () VALUES ")
-
-
+    # cursor.execute("INSERT INTO CART () VALUES ")
 
 
-    return render_template('cart.html', purches = rows)
+
+
+    return render_template('cart.html', purchase = rows)
+
+@app.route('/delete_product', methods = ['POST'])
+def delete_part():
+
+    if request.form:
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        orderid = request.form['orderid']
+        orderid = request.form['orderid']
+        cursor.execute("SELECT * from parts WHERE orderid = %s", (orderid,))
+        order = cursor.fetchone()
+        partid = order['partid']
+        id = session['userid']
+        print(orderid)
+        print(id)
+        print(partid)
+
+
+        cursor.execute("DELETE FROM purchase WHERE partid = (%s) AND userid = (%s) AND orderid = (%s)", (partid, id, orderid,))
+        rows_deleted = cursor.rowcount
+        conn.commit()
+        return redirect(url_for('cart'))
+
     
 
     
 
 
-@app.route('/delete_from_cart/id',  methods=['GET', 'POST'])
-def delete_from_cart(id):
-    orderid = request.form['orderid']
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    
-    
-    cursor.execute("DELETE FROM purches WHERE partid = %s", (id))
-    conn.commit()
-
-    return redirect(url_for('cart'))
 
 
-@app.route('/checkout')
+@app.route('/checkout.html',  methods=['GET', 'POST'])
 def checkout():
 
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    userid = session['userid']
+    cursor.execute("SELECT * from purchase WHERE userid = %s ", (userid,))
+    rows = cursor.fetchall()
+ 
+    
+
+    if ( (request.method == 'POST') and ('name' in request.form) and ('card' in request.form) and ('date' in request.form) and ('cvc' in request.form)):
+        
+        
+           cartid = '1'
+           userid = session['userid']
+           name_on_card = request.form['name']
+           card_number = request.form['card']
+           date = request.form['date']
+           cvc = request .form['cvc']
+
+           cursor.execute("INSERT INTO billing (card_name, card_number, exp_date, cvc, cartid) VALUES (%s, %s, %s, %s, %s)", (name_on_card, card_number, date, cvc, userid))
+           conn.commit()
+
+           cursor.execute("DELETE FROM purchase WHERE userid = %s", (userid,))
+           rows_deleted = cursor.rowcount
+           conn.commit()
+           return redirect(url_for('index'))
+    
+        
 
 
-    return render_template()
+
+
+        
+
+
+
+
+
+
+
+
+    return render_template('checkout.html', purchase = rows)
 
 
 
 if __name__ == "__main__":
+    
+
     app.run(debug = True)
